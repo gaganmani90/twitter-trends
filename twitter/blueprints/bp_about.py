d5eee179f2680136f247719d9fc5d4f7d46997bd
Shivani <shivanidwivedi168@gmail.com>
2020-05-26 12:50:08 -0700

    experiment with buttons

--- twitter/blueprints/bp_about.py
+++ twitter/blueprints/bp_about.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
 
+from util.location_util import woeid_to_location_map, location_models
+
 about = Blueprint('about', __name__,
                   template_folder='templates')
 
 
 @about.route("/about")
 def show_about():
-    return render_template("about.html")
+    locations = woeid_to_location_map()
+    locations_by_parent = location_models()
+    return render_template("about.html", map=locations, locations_by_parent=locations_by_parent)
+
+
+
